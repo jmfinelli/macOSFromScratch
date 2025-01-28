e92a0b0d1a3beee0a3ab99bd0d18a94615b483a3
Manuel Finelli <jfinelli@redhat.com>
2025-01-28 12:46:24 +0000

    Created backup script

---
+++ backup.sh
@@ -0,0 +1,10 @@
+rm -rf ~/backup
+mkdir -p ~/backup
+
+cp -r ~/.ssh ~/backup/ssh
+cp -r ~/.gnupg ~/backup/gnup
+cp ~/.gitconfig ~/backup/gitconfig
+cp -r ~/.logseq ~/backup/logseq
+cp ~/.vimrc ~/backup/vimrc
+cp ~/.zshrc ~/backup/zshrc
+
